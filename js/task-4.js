98e085d2a68ee818ffb3c6b2fe826d1b63898920
Serhii Mordvaniuk <sm636332@gmail.com>
2024-10-05 15:40:13 +0200

    4 task

--- js/task-4.js
+++ js/task-4.js
@@ -1,2 +1,17 @@
 "use strict"
 
+const form = document.querySelector(".login-form")
+const user = [];
+
+form.addEventListener("submit", (event) => {
+    if (event.target.elements.email.value == "" || event.target.elements.password.value == "") {
+        alert("All form fields must be filled in")
+    }
+    else {
+        event.preventDefault()
+        user.email = event.target.elements.email.value;
+        user.password = event.target.elements.password.value;
+        console.log(user);
+        event.target.reset()
+    }
+})
