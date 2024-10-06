9f4be562ae1291b709f42284eebbf27aa36a4e40
Serhii Mordvaniuk <sm636332@gmail.com>
2024-10-06 12:07:24 +0200

    fix 4

--- js/task-4.js
+++ js/task-4.js
@@ -4,11 +4,11 @@ const form = document.querySelector(".login-form")
 const user = [];
 
 form.addEventListener("submit", (event) => {
+        event.preventDefault()
     if (event.target.elements.email.value == "" || event.target.elements.password.value == "") {
         alert("All form fields must be filled in")
     }
     else {
-        event.preventDefault()
         user.email = event.target.elements.email.value;
         user.password = event.target.elements.password.value;
         console.log(user);
